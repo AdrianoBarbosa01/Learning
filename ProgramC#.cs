da07d515fd28b4154a3ba88b4d7b5831c93e2a9f
AdrianoBarbosa01 <adriano.barbosa21@etec.sp.gov.br>
2022-12-01 13:36:46 -0300

    Create ProgramC#.cs

---
+++ ProgramC#.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learning
+{
+    internal static class Program
+    {
+
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Learning the C# language!");
+        }
+    }
+}
